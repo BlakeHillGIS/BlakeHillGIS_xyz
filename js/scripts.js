5628bf3ce17041bee732ab133e45736ef22242b6
Blake Hill <192072257+BlakeHillGIS@users.noreply.github.com>
2025-08-01 16:21:03 -0500

    Update scripts.js
    not using navbar or shrink functions, removed

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,20 @@
 /*!
- * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
- * Copyright 2013-2023 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
- */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
-/*!
- * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
- * Copyright 2013-2020 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
- */
-(function ($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top - 72,
-          },
-          1000,
-          "easeInOutExpo",
-        );
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function () {
-    $(".navbar-collapse").collapse("hide");
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $("body").scrollspy({
-    target: ".navbar",
-    offset: 74,
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function () {
-    if ($(".navbar").offset().top > 100) {
-      $(".navbar").addClass("navbar-shrink");
-    } else {
-      $(".navbar").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-})(jQuery); // End of use strict
-
-
+* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
 
 // -----testing adding floating return to top button
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
 // function scrollFunction() {
 //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
