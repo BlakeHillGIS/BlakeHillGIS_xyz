b91cd6b2b4be2748cb169e3eaf3a744405778817
Blake Hill <192072257+BlakeHillGIS@users.noreply.github.com>
2025-08-01 21:50:33 -0500

    add images
    add to density maps

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@
 
 // -----testing adding floating return to top button
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+// let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 // window.onscroll = function () {
@@ -17,7 +17,8 @@ let mybutton = document.getElementById("myBtn");
 // };
 
 // function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//   if (document.body.scrollTop > 20 ||
+// document.documentElement.scrollTop > 20) {
 //     mybutton.style.display = "block";
 //   } else {
 //     mybutton.style.display = "none";
